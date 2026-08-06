52795f353831d4ccbf4e2aac7a68ea5923f08f7a
SergeyKolobovDeveloper <serglra2007@gmail.com>
2026-08-06 17:01:58 +0300

    initial commit / Початкова структура сайту (шапка та пiдвал), а також доданi стилi CSS

---
+++ includes/aside.php
@@ -0,0 +1,8 @@
+<aside>
+    <div class="info">
+        <h2>Цікаві факти!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sint at, maxime suscipit quisquam cumque nostrum molestiae consequatur odio laboriosam corrupti. Quo deleniti ipsum accusantium veritatis aperiam. Similique, alias magnam!</p>
+    </div>
+    <img src="/img/Antarctica.jpg" alt="jpg">
+</aside>
+    
---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+    <span class="logo">Blog Master</span>
+    <nav>
+        <a href="/">Головна</a>
+        <a href="/login.php" class="btn">Увійти</a>
+        <a href="/register.php" class="btn">Рейстрація</a>
+    </nav>
+</header>
