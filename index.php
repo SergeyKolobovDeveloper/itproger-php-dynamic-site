dea17acaee8d1809cc237de568c38fa5ca15cf8d
SergeyKolobovDeveloper <serglra2007@gmail.com>
2026-08-17 16:42:33 +0300

    Вивiд усiх статей з бази даних

--- index.php
+++ index.php
@@ -3,7 +3,20 @@ $website_title = "Blog Master";
 require "blocks/header.php";
 ?>
     <main>
-        <p>Main text</p>
+        <?php
+        require_once 'lib/mysql.php';
+
+        $sql = 'SELECT * FROM `articles` ORDER BY `date` DESC';
+        $query = $pdo->query($sql);
+        while($row = $query->fetch(PDO::FETCH_OBJ)) {
+            echo "<div class='post'>
+            <h1>". $row->title ."</h1>
+            <p>". $row->anons ."</p>
+            <p class='author'>Автор: <span>". $row->author ."</span></p>
+            <a href='/'>Читати</a>
+            </div>";
+        }
+        ?>
     </main>
 <?php require "blocks/aside.php" ?>
 <?php require "blocks/footer.php" ?>
